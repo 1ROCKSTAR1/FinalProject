be48a5136166652cc5d274784be5ac6e3d9a9f46
alexaastri <AVoronin@lanit.ru>
2026-01-30 15:25:41 +0300

    some changes for mobile tests were added (old habitica app, new androidDriver)

--- src/test/java/mobile/tests/Tests.java
+++ src/test/java/mobile/tests/Tests.java
@@ -1,16 +1,11 @@
 package mobile.tests;
 
-import io.appium.java_client.AppiumBy;
 import mobile.base.BaseEmulatorTest;
 import mobile.screens.GreetingScreen;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
-import static io.qameta.allure.Allure.step;
 
 @Tag("mobile")
 public class Tests extends BaseEmulatorTest {
